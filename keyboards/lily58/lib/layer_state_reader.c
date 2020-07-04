4f0d96308fa3b98e6e4e2280f050dee99ef0a11d
ArS <>
2020-07-04 16:07:52 +0900

    change oled layer message to mine

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -3,29 +3,29 @@
 #include <stdio.h>
 #include "lily58.h"
 
-#define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+#define _QWERTY 0
+#define _SYMBOL (1 << 1)
+#define _MOVING (1 << 2)
+#define _TENKEY (1 << 3)
+#define L_ADJUST_TRI (_TENKEY | _MOVING | _SYMBOL)
 
 char layer_state_str[24];
 
 const char *read_layer_state(void) {
   switch (layer_state)
   {
-  case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+  case _QWERTY:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: QWERTY");
     break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+  case _MOVING:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Moving");
     break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+  case _SYMBOL:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Symbols");
     break;
-  case L_ADJUST:
+  case _TENKEY:
   case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: 10Key");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
