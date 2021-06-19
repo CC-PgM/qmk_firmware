6a593a8cc0284d70de273cdfc27dfcabdc6d3af9
ArS <22502259+sigref@users.noreply.github.com>
2021-06-19 19:04:42 +0900

    何か変更

--- keyboards/lily58/keymaps/custom/keymap.c
+++ keyboards/lily58/keymaps/custom/keymap.c
@@ -41,16 +41,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |SYMBOL| /Space  /       \Enter \  |MOVING|BackSP|  =+  |
- *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   |      |      |LNG_EN|/       /         \      \ |LNG_JA|      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT( \
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_CAPS, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_F13,   KC_F14,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS, \
-                          KC_LALT, KC_LGUI,  SYMBOL, KC_SPC,   KC_ENT,   MOVING,  KC_BSPC, KC_EQL \
+              KC_LALT, KC_LGUI, LT(_SYMBOL, KC_F13), KC_SPC,   KC_ENT, LT(_MOVING, KC_F14), KC_BSPC, KC_EQL \
 ),
 /* SYMBOL
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -62,8 +62,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | TRNS | TRNS |SYMBOL| / TRNS  /       \ TRNS \  |TENKEY| TRNS | DEL  |
- *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   | TRNS | TRNS | TRNS | / TRNS  /       \ TRNS \  |  TO  | TRNS | DEL  |
+ *                   |      |      |      |/       /         \      \ |TENKEY|      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_SYMBOL] = LAYOUT( \
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
   KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-                             _______, _______, _______, _______, _______,  TENKEY, _______, KC_DEL \
+                             _______, _______, _______, _______, _______, TO(_TENKEY), _______, KC_DEL \
 ),
 /* MOVING
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -83,17 +83,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|      |      |      |Insert|      |-------|    |-------|      |      |      |      |      |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | TRNS | TRNS |TENKEY| / TRNS  /       \ TRNS \  | TRNS | TRNS | DEL  |
- *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   | TRNS | TRNS |  TO  | / TRNS  /       \ TRNS \  | TRNS | TRNS | DEL  |
+ *                   |      |      |TENKEY|/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_MOVING] = LAYOUT( \
-  KC_ESC,   XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC, KC_DEL,                     KC_BSPC, KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  KC_TAB,   XXXXXXX, KC_UP,   XXXXXXX, KC_HOME, KC_END,                     KC_HOME, KC_END,  KC_PGUP, KC_PGDN, XXXXXXX, XXXXXXX, \
-  KC_LCTRL, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_RCTL, \
-  KC_LSFT,  XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT, \
-                              _______, _______, TENKEY,  _______, _______,  _______, _______, _______ \
+  KC_ESC,   XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC, KC_DEL,                    KC_BSPC, KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  KC_TAB,   XXXXXXX, KC_UP,   XXXXXXX, KC_HOME, KC_END,                    KC_HOME, KC_END,  KC_PGUP, KC_PGDN, XXXXXXX, XXXXXXX, \
+  KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_RCTL, \
+  KC_LSFT,  XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT, \
+                          _______, _______, TO(_TENKEY), _______, _______, _______, _______, _______ \
 ),
 /* TENKEY
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -105,16 +105,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  F13  |    |  F14  |------+------+------+------+------+------|
  * |LShift|      |      |      |      |      |-------|    |-------|      |  n0  |  .   |  ,   | Enter|RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | TRNS | TRNS |QWERTY| / TRNS  /       \ TRNS \  |QWERTY| TRNS | TRNS |
- *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   | TRNS | TRNS |  TO  | / TRNS  /       \ TRNS \  |  TO  | TRNS | TRNS |
+ *                   |      |      |QWERTY|/       /         \      \ |QWERTY|      |      |
  *                   `----------------------------'           '------''--------------------'
  */
   [_TENKEY] = LAYOUT( \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PAST, KC_PSLS, \
   XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PMNS, \
   XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, KC_RCTL, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P0,   KC_PDOT, KC_PCMM, KC_PENT, KC_RSFT, \
-                             _______, _______, QWERTY,  _______, _______, QWERTY,  _______, _______ \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F13,  KC_F14,  XXXXXXX, KC_P0,   KC_PDOT, KC_PCMM, KC_PENT, KC_RSFT, \
+                         _______, _______, TO(_QWERTY), _______, _______, TO(_QWERTY), _______, _______ \
   )
 };
 
